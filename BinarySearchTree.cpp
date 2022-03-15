ec2d2838ab24d46e8646fc75a6601b24fc91f569
bestytode <90923322+bestytode@users.noreply.github.com>
2022-03-15 21:57:15 +0800

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,8 +1,7 @@
-#include"BinaryTree.h"
-#include<iostream>
+#include"BST.h"
 using namespace std;
 
-/*	测试用例结构
+/*	Test Tree Structure
 		 5
 	   /   \
 	  2     8
@@ -14,30 +13,29 @@ using namespace std;
 
 int main()
 {
-	BinaryTree tree = { 5, 8, 6, 7, 2, 3, 1, 9, 4 };
+	BinarySearchTree tree = { 5, 8, 6, 7, 2, 3, 1, 9, 4 };
 
-	//得到 5 2 1 3 4 8 6 7 9
+	// Get 5 2 1 3 4 8 6 7 9
 	tree.print(TraverseFlag::PRE_ORDER);
 	cout << endl;
 
-	//得到 1 2 3 4 5 6 7 8 9
+	// Get 1 2 3 4 5 6 7 8 9
 	cout << tree << endl;
 
-	//得到 1 4 3 2 7 6 9 8 5
+	// Get 1 4 3 2 7 6 9 8 5
 	tree.print(TraverseFlag::POST_ORDER);
 	cout << endl;
 
-    //得到 5 2 8 1 3 6 9 4 7 
+        // Get 5 2 8 1 3 6 9 4 7 
 	tree.print(TraverseFlag::LEVEL_ORDER);
 	cout << endl;
 
 	//tree.erase(5);
 	tree.insert(10);
 
-	//得到 1 2 3 4 6 7 8 9 10
-	cout << tree;
-	cout << endl;
-
+	// Get 1 2 3 4 6 7 8 9 10
+	cout << tree << endl;
+	
 	cout << "minValue = " << tree.minValue() << endl;
 	cout << "maxValue = " << tree.maxValue() << endl;
 }
