281c3eb13ff798d7e3b7a698c53f7333e4999918
bestytode <90923322+bestytode@users.noreply.github.com>
2022-03-23 00:29:40 +0800

    Update LinkedList.h

--- LinearList/LinkedList.h
+++ LinearList/LinkedList.h
@@ -1,108 +1,260 @@
-// template TODO LinkedList
-
 #pragma once
-#ifndef MY_LINKED_LIST_
-#define MY_LINKED_LIST_
+#ifndef LinkedList_
+#define LinkedList_
 
-#include<iostream>
 #include<initializer_list>
-#include<cassert>
+#include<iostream>
+#include<stdexcept>
 
-// Struct ListNode with datatype integer
-struct ListNode
+template<typename T>
+struct Node
 {
-	int val;
-	ListNode* next;
+	T val;
+	Node<T>* next;
 
-	ListNode() :val(0), next(nullptr) {};
-	ListNode(int  data) :val(data), next(nullptr) {};
-	ListNode(int  data, ListNode* next) : val(data), next(next) {};
+	Node() :val(0), next(nullptr) {};
+	Node(T val) :val(val), next(nullptr) {};
+	Node(T val, Node<T>* node) : val(val), next(node) {};
 
-	friend std::ostream& operator <<(std::ostream& out, const ListNode& node)
-	{	
-	    return out << node.val;		
+	friend std::ostream& operator <<(std::ostream& out, const Node<T>& node)
+	{
+		return out << node.val;
 	}
 };
 
-// Class LinkedList with datatype integer
+template<typename T>
 class LinkedList final
 {
 public:
-	LinkedList() : head(nullptr), m_size(0) {}
-	LinkedList(std::initializer_list<int>list)
+	// Default constructor
+	LinkedList() : 
+		head(nullptr), m_size(0) {}
+
+	LinkedList(std::initializer_list<T> list)
 	{
-		for (int data : list)
-			push_back(data);
+		for (T val : list)
+			push_back(val);
 	}
 
-	// Copy constuctor
-	LinkedList(const LinkedList& list)
+	// Copy constructor
+    LinkedList(const LinkedList& other)	
 	{
-		//TODO
+		if (other.empty())
+		{
+			head = nullptr;
+			m_size = 0;
+		}
+		
+		head = new Node<T>(other.head->val);
+		Node<T>* current = head;
+		Node<T>* otherHead = other.head;
+		Node<T>* otherCurrent = otherHead;
+
+		// if other->next exists, copy the same value to current node
+		while (otherCurrent->next) {
+			current->next = new Node<T>(otherCurrent->next->val);
+			otherCurrent = otherCurrent->next;
+			current = current->next;
+		}
+		m_size = other.m_size;
 	}
 
-	int& operator[](int index) const
+	T& operator[](size_t index)const
 	{
-		//TODO
+		if (empty() || index > m_size - 1)
+			throw std::invalid_argument("Invalid index!");
+
+		Node<T>* temp = head;
+		for (size_t i = 0; i < index; i++)
+		{
+			temp = temp->next;
+		}
+		return temp->val;
 	}
 
-	void push_front(int val)
+	void push_front(T val)
 	{
-		//TODO
+		Node<T>* temp = new Node<T>(val);
+		if (empty())
+		{
+			head = temp;
+			m_size++;
+			return;
+		}
+
+		temp->next = head;
+		head = temp;
+		m_size++;
 	}
 
-	void push_back(int val)
+	void push_back(T val) 
 	{
-		//TODO
+		if (empty())
+		{
+			push_front(val);
+			return;
+		}
+
+		Node<T>* temp = head;
+        Node<T>* newNode = new Node<T>(val);
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = newNode;
+		m_size++;
 	}
-        
+
 	void pop_front()
 	{
-		//TODO
+		if (empty())
+			return;
+
+		Node<T>* temp = head;
+		head = head->next;
+		delete temp;
+		--m_size;
 	}
-	
+
 	void pop_back()
 	{
-		//TODO
-	}
-	
-	void insert(int val, int index)
-	{
-		//TODO
+		if (empty())
+		{
+			return;
+		}
+		else if (m_size == 1)
+		{
+			pop_front();
+			return;
+		}
+
+		Node<T>* prev = head;
+		Node<T>* curr = head->next;
+		while (curr->next)
+		{
+			prev = prev->next;
+			curr = curr->next;
+		}
+
+		// the prev now becomes the last node
+		// so the next pointer of the prev point to nullptr
+		prev->next = nullptr;
+		delete curr;
+		--m_size;
 	}
 
-	void erase(int index)
+	void insert(int val, size_t index)
 	{
-		//TODO
+
+		if (empty() || index > m_size)
+			throw std::invalid_argument("Invalid index!");
+
+		// If inserting a new head
+		if (index == 0)
+		{
+			push_front(val);
+			return;
+		}
+		// If inserting a new last listNode
+		else if (index == m_size)
+		{
+			push_back(val);
+			return;
+		}
+
+		Node<T>* prev = head;
+
+		// Traverse the elements until the selected index is found
+		for (size_t i = 0; i < index - 1; ++i)
+		{
+			prev = prev->next;
+		}
+		Node<T>* next = prev->next;
+		Node<T>* newNode = new Node<T>(val);
+
+		// Insert this new node between the previous node and the next node
+		newNode->next = next;
+		prev->next = newNode;
+		++m_size;
 	}
 
-	void reverse()
+	void erase(size_t index)
 	{
-		//TODO
+		if (empty() || index > m_size - 1)
+			throw std::invalid_argument("Invalid index!");
+
+		if (empty())
+		{
+			return;
+		}
+		else if (index == 0)
+		{
+			pop_front();
+			return;
+		}
+		else if (index == m_size - 1)
+		{
+			pop_back();
+			return;
+		}
+
+		Node<T>* prev = head;
+		for (size_t i = 0; i < index - 1; ++i)
+		{
+			prev = prev->next;
+		}
+
+		Node<T>* curr = prev->next;
+		Node<T>* next = curr->next;
+
+		// Link the prev to next to remove the mid element
+		prev->next = next;
+		delete curr;
+		--m_size;
 	}
 
-	void sort()
+	void reverse()	
 	{
-		//TODO
-	}
-	
-	int size() const
-	{
-		//TODO
-	}
-	
-        void empty() const
-	{
-		//TODO
-	}
-	
-	// Overloading << to output whole LinkedList
-	friend std::ostream& operator<<(std::ostream& out, const LinkedList& list)
-	{
-		if(list.empty())
-		        return out;
+		if (empty() || size() == 1)
+			return;
 
-                ListNode* temp = list.head;
+		Node<T>* prev = nullptr;
+		Node<T>* curr = head;
+		while (curr)
+		{
+			Node<T>* next = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
+	}
+
+	//Utilities
+	void print()const
+	{
+		std::cout << *this;
+	}
+
+	int size()const
+	{
+		return m_size;
+	}
+
+	bool empty()const
+	{
+		return m_size == 0;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const LinkedList& list)
+	{
+		if (list.empty())
+		{
+			return out;
+		}
+
+		Node<T>* temp = list.head;
 		while (temp)
 		{
 			out << temp->val << " ";
@@ -110,58 +262,50 @@ public:
 		}
 		return out;
 	}
-
-
 private:
-	ListNode* head;
-	int m_size = 0;
-
+	Node<T>* head;
+	size_t m_size = 0;
 public:
-
-	// Class Iterator with datatype integer
 	class Iterator
 	{
 	public:
+		Iterator() : current(nullptr) {}
+		Iterator(Node<T>* node) : current(node) {}
 
-		Iterator() :current(nullptr) {}
-		Iterator(ListNode* node) :current(node) {}
-
-		// Dereference 
+		// Dereference operation
 		int& operator*()
 		{
 			return current->val;
 		}
 
-		Iterator& operator++()
+		Iterator& operator ++ ()
 		{
 			current = current->next;
 			return *this;
 		}
 
-		Iterator operator++(int)
+		Iterator operator ++ (int)
 		{
-			Iterator old = *this;
+			Iterator itr = *this;
 			current = current->next;
-			return old;
+			return itr;
 		}
 
-		//Iterator & operator -- ();	// No this method in singly list
-		//Iterator operator -- (int);   // No this method in singly list
+		//Iterator & operator -- ();	// No this method existing in singly list
+		//Iterator operator -- (int);   // No this method existing in singly list
 
-		bool operator == (const Iterator & itr)
+		bool operator == (const Iterator& other)
 		{
-			return current == itr.current;
+			return current == other.current;
 		}
 
-		bool operator != (const Iterator& itr)
+		bool operator != (const Iterator& other)
 		{
-			return current != itr.current;
+			return current != other.current;
 		}
-
 	private:
-		ListNode* current;
+		Node<T>* current;
 	};
-
 public:
 	Iterator begin() 
 	{
@@ -170,14 +314,13 @@ public:
 
 	Iterator end()
 	{
-		Iterator it = Iterator(head);
+		Iterator itr = Iterator(head);
 
-		for (int i = 0; i < m_size; i++)
-		{
-			it++;
+		for (size_t i = 0; i < m_size; i++) {
+			itr++;
 		}
-		return it;
+		return itr;
 	}
 };
 
-#endif MY_LINKED_LIST_
+#endif LinkedList_
